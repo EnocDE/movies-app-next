25074f05a2e487b74b438a8b1755e8e899e246b7
Enoc Dominguez <enocescazan@hotmail.com>
2024-07-21 22:31:33 -0600

    Add navbar component to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { NextUIProvider } from "@nextui-org/react";
+import Nav from "./components/Nav";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <NextUIProvider>
+          <Nav />
           {children}
         </NextUIProvider>
       </body>
