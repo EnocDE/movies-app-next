d1aec9a31b086b60e4ed5bc76edc4c382b3cb956
Enoc Dominguez <enocescazan@hotmail.com>
2024-07-21 22:30:53 -0600

    Add navbar component

---
+++ app/components/Nav/index.tsx
@@ -0,0 +1,74 @@
+"use client";
+import { useState } from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  NavbarMenuToggle,
+  NavbarMenu,
+  NavbarMenuItem,
+  Link,
+  Button,
+} from "@nextui-org/react";
+import { menuItems, menuPrincipalItems } from "./nav.data";
+import { useParams, usePathname, useRouter } from "next/navigation";
+
+export default function Nav() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const pathname = usePathname()
+  
+  return (
+    <Navbar onMenuOpenChange={setIsMenuOpen}>
+      <NavbarContent>
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          className="sm:hidden"
+        />
+        <NavbarBrand>
+          <p className="font-bold text-inherit">ACME</p>
+        </NavbarBrand>
+      </NavbarContent>
+
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        {menuPrincipalItems.map(({name, url}) => (
+          <NavbarItem key={name}>
+            <Link color={pathname === url ? "primary" : "foreground"} href={url}>
+              {name}
+            </Link>
+          </NavbarItem>
+        ))}
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem className="hidden lg:flex">
+          <Link href="#">Login</Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Button as={Link} color="primary" href="#" variant="flat">
+            Sign Up
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarMenu>
+        {menuItems.map(({name, url}, index) => (
+          <NavbarMenuItem key={name}>
+            <Link
+              color={
+                index === 2
+                  ? "primary"
+                  : index === menuItems.length - 1
+                  ? "danger"
+                  : "foreground"
+              }
+              className="w-full"
+              href={url}
+              size="lg"
+            >
+              {name}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+    </Navbar>
+  );
+}
---
+++ app/components/Nav/nav.data.ts
@@ -0,0 +1,61 @@
+export const menuItems = [
+  {
+    name: "Profile",
+    url: ""
+  },
+  {
+    name: "Dashboard",
+    url: ""
+  },
+  {
+    name: "Activity",
+    url: ""
+  },
+  {
+    name: "Analytics",
+    url: ""
+  },
+  {
+    name: "System",
+    url: ""
+  },
+  {
+    name: "Deployments",
+    url: ""
+  },
+  {
+    name: "My Settings",
+    url: ""
+  },
+  {
+    name: "Team Settings",
+    url: ""
+  },
+  {
+    name: "Help & Feedback",
+    url: ""
+  },
+  {
+    name: "Log Out",
+    url: ""
+  },
+];
+
+export const menuPrincipalItems = [
+  {
+    name: "Home",
+    url: "/"
+  },
+  {
+    name: "Movies",
+    url: "/movies"
+  },
+  {
+    name: "Series",
+    url: "/series"
+  },
+  {
+    name: "My list",
+    url: "/my-list"
+  },
+]
