a8e8048c24bc5e39a557f1a5b945994edd5e9025
Enoc Dominguez <enocescazan@hotmail.com>
2024-07-24 14:48:44 -0600

    Add MenuSelectionHeader component

---
+++ components/MenuSelectionHeader/index.tsx
@@ -0,0 +1,33 @@
+import { MenuSelectionType } from "@/types/menu-selection";
+import { Dispatch, SetStateAction } from "react";
+
+interface MenuSelectionHeaderProps {
+  setOptionState: Dispatch<SetStateAction<string>>;
+  optionState: string;
+  menuSelection: MenuSelectionType[];
+}
+
+export default function MenuSelectionHeader(props: MenuSelectionHeaderProps) {
+  const { setOptionState, optionState, menuSelection } = props;
+
+  const handleSetOption = (value: string) => setOptionState(value);
+  return (
+    <div>
+      <header className="mx-auto max-w-[80%] flex gap-10 justify-evenly md:justify-between items-center border-b-1 flex-wrap py-10">
+        {menuSelection.map((selection) => (
+          <h3
+            key={selection.name}
+            className={`hover:cursor-pointer transition-all select-none text-medium ${
+              optionState === selection.url
+                ? "scale-150 block md:w-fit font-bold"
+                : ""
+            }`}
+            onClick={() => handleSetOption(selection.url)}
+          >
+            {selection.name}
+          </h3>
+        ))}
+      </header>
+    </div>
+  );
+}
