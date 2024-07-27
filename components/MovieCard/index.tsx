34d188d5a7b833017a205b715d5ddf54b0e525d8
Enoc Dominguez <enocescazan@hotmail.com>
2024-07-26 20:40:53 -0600

    Add movie information on hover of the card

--- components/MovieCard/index.tsx
+++ components/MovieCard/index.tsx
@@ -1,5 +1,6 @@
 import { MovieType } from "@/types/movies-response";
-import { Image, Skeleton } from "@nextui-org/react";
+import { truncateRating } from "@/utils";
+import { Button, Image, Skeleton } from "@nextui-org/react";
 import Link from "next/link";
 import React, { RefObject } from "react";
 
@@ -10,31 +11,61 @@ interface MovieCardProps {
 
 export default function MovieCard(props: MovieCardProps) {
   const { movie, movieCardRef } = props;
+
+  const handleAddToFavorites = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.stopPropagation();
+    console.log("Añadiendo a favoritos");
+  };
+
+  const handleGoToMoviePage = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.stopPropagation();
+    console.log("Abriendo pelicula");
+  };
+
   return movie ? (
     <li
-      ref={movieCardRef}
+      ref={movieCardRef || null}
       key={movie.id}
-      className="w-72 snap-center overflow-hidden flex-shrink-0"
+      className="w-72 snap-center flex-shrink-0"
     >
-      <Link href={"#"} className="relative w-full h-full">
-        <div className="overflow-hidden rounded-xl">
-          <Image
-            removeWrapper
-            alt="Movie poster"
-            className="w-full h-full object-cover hover:scale-105"
-            src={`${
-              process.env.NEXT_PUBLIC_TMDB_IMAGES
-            }/t/p/w500/${movie.poster_path!}`}
-          />
-        </div>
-        <div>
-          <h3 className="font-bold">{movie.title}</h3>
-          <div className="flex justify-between">
-            <p className="text-tiny">{movie.release_date}</p>
-            <p className="text-tiny">⭐{movie.vote_average.toFixed(1)}</p>
+      <div className="relative rounded-xl group">
+        <Image
+          removeWrapper
+          alt="Movie poster"
+          src={`${
+            process.env.NEXT_PUBLIC_TMDB_IMAGES
+          }/t/p/w500/${movie.poster_path!}`}
+        />
+        <div className="absolute inset-0 bg-black/50 opacity-0 p-5 group-hover:opacity-100 z-10 text-white rounded-xl backdrop-blur-md transition-all flex flex-col">
+          <Link href={`/movie/${movie.id}`}>
+            <h3 className="font-bold mb-5">{movie.title}</h3>
+          </Link>
+          <p className="text-tiny text-neutral-300 text-pretty flex-1">
+            {movie.overview}
+          </p>
+          <div className="flex justify-between gap-5">
+            <Button className="flex-1" onClick={handleAddToFavorites}>
+              ❤️
+            </Button>
+            <Button className="flex-1" onClick={handleGoToMoviePage}>
+              ➕
+            </Button>
           </div>
         </div>
-      </Link>
+      </div>
+      <div>
+        <Link href={`/movie/${movie.id}`}>
+          <h3 className="font-bold">{movie.title}</h3>
+        </Link>
+        <div className="flex justify-between">
+          <p className="text-tiny">{movie.release_date}</p>
+          <p className="text-tiny">⭐{truncateRating(movie.vote_average)}</p>
+        </div>
+      </div>
     </li>
   ) : (
     <li ref={movieCardRef} className="flex-shrink-0 rounded-xl space-y-2">
