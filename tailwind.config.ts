f56248802a823ca3134eab73afab022c2045c5d4
Enoc Dominguez <enocescazan@hotmail.com>
2024-07-27 19:05:44 -0600

    Add 2 gradients and grid auto-columns

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,7 +9,15 @@ const config: Config = {
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "gradient-btt-h": "linear-gradient(90deg, rgba(0,0,0,.5) 0%, rgba(0,0,0,0) 3%, rgba(0,0,0,0) 50%, rgba(0,0,0,0) 97%, rgba(0,0,0,.5) 100%)",
+        "gradient-btw-h": "linear-gradient(90deg, rgba(255,255,255,0.5) 0%, rgba(0,0,0,0) 3%, rgba(0,0,0,0) 50%, rgba(0,0,0,0) 97%, rgba(255,255,255,0.5) 100%)",
+      },
+      gridTemplateColumns: {
+        "auto-fit": "repeat(auto-fit, 350px)",
+      }
+    },
   },
   plugins: [nextui()],
   darkMode: "class",
