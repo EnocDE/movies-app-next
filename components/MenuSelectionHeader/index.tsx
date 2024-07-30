fdb853a02b5e3c6e16123952dc71219339c3c0ee
Enoc Dominguez <enocescazan@hotmail.com>
2024-07-29 21:45:20 -0600

    Update prop type

--- components/MenuSelectionHeader/index.tsx
+++ components/MenuSelectionHeader/index.tsx
@@ -2,27 +2,27 @@ import { MenuSelectionType } from "@/types/menu-selection";
 import { Dispatch, SetStateAction } from "react";
 
 interface MenuSelectionHeaderProps {
-  setOptionState: Dispatch<SetStateAction<string>>;
-  optionState: string;
+  setOptionState: Dispatch<SetStateAction<"movie" | "tv">>;
+  optionState: "movie" | "tv";
   menuSelectionData: MenuSelectionType[];
 }
 
 export default function MenuSelectionHeader(props: MenuSelectionHeaderProps) {
   const { setOptionState, optionState, menuSelectionData } = props;
 
-  const handleSetOption = (value: string) => setOptionState(value);
+  const handleSetOption = (value: "movie" | "tv") => setOptionState(value);
   return (
     <div>
-      <header className="mx-auto max-w-[80%] flex gap-10 justify-evenly md:justify-between items-center border-b-1 flex-wrap py-10">
+      <header className="mx-auto max-w-[80%] flex gap-10 justify-evenly items-center border-b-1 flex-wrap py-10">
         {menuSelectionData.map((selection) => (
           <h3
             key={selection.name}
             className={`hover:cursor-pointer transition-all select-none text-medium ${
-              optionState === selection.url
+              optionState === selection.type
                 ? "scale-150 block md:w-fit font-bold"
                 : ""
             }`}
-            onClick={() => handleSetOption(selection.url)}
+            onClick={() => handleSetOption(selection.type)}
           >
             {selection.name}
           </h3>
