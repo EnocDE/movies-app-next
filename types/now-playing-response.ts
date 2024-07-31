08113dcdb3dfb6c0a144bac4483dea90bdba5b71
Enoc Dominguez <enocescazan@hotmail.com>
2024-07-30 21:36:04 -0600

    Update tab spaces

--- types/now-playing-response.ts
+++ types/now-playing-response.ts
@@ -2,7 +2,7 @@ import z from "zod"
 import { MoviesSchema } from "./movies-response"
 
 export const MoviesNowPlayingDatesSchema = z.object({
-   maximum: z.string(),
+  maximum: z.string(),
   minimum: z.string()
 })
 
