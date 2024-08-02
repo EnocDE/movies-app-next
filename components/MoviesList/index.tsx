92a3cdb3b3d7da05ba90a598c80272c570591bd0
Enoc Dominguez <enocescazan@hotmail.com>
2024-08-02 16:58:03 -0600

    Update component, props, variables and constants name

--- components/MoviesList/index.tsx
+++ components/MoviesList/index.tsx
@@ -1,43 +1,42 @@
 "use client";
 
-import { MoviesResultsSchema, MoviesSchema, MovieType } from "@/types/movies-response";
+import { MoviesResultsSchema, MovieType } from "@/types/movies-response";
 import { SerieType } from "@/types/series-response";
 import { useRef } from "react";
-import MovieCard from "../MovieCard";
+import ItemCard from "../MovieCard";
 
-interface MoviesListProps {
-  movies: undefined | unknown;
+interface ItemsListProps {
+  uknownItems: undefined | unknown;
   classNames?: string;
 }
 
-export default function MoviesList(props: MoviesListProps) {
-  const { movies, classNames } = props;
-  
-  let items : SerieType[] | MovieType[];
-  if (MoviesResultsSchema.safeParse(movies).success) {
-    items = movies as MovieType[];
+export default function ItemsList(props: ItemsListProps) {
+  const { uknownItems, classNames } = props;
+
+  let items: SerieType[] | MovieType[];
+  if (MoviesResultsSchema.safeParse(uknownItems).success) {
+    items = uknownItems as MovieType[];
   } else {
-    items = movies as SerieType[];
+    items = uknownItems as SerieType[];
   }
 
-  const movieCardRef = useRef<HTMLLIElement>(null);
-  const moviesListRef = useRef<HTMLUListElement>(null);
+  const itemCardRef = useRef<HTMLLIElement>(null);
+  const itemsListRef = useRef<HTMLUListElement>(null);
   const movieListContainerRef = useRef<HTMLDivElement>(null);
 
-  const slideMoviesListToRight = () => {
-    if (moviesListRef.current && movieCardRef.current) {
-      const widthCard = movieCardRef.current.offsetWidth + 40;
-      moviesListRef.current.scrollBy({ left: widthCard, behavior: "smooth" });
+  const slideItemsListToRight = () => {
+    if (itemsListRef.current && itemCardRef.current) {
+      const widthCard = itemCardRef.current.offsetWidth + 40;
+      itemsListRef.current.scrollBy({ left: widthCard, behavior: "smooth" });
     }
   };
 
-  const slideMoviesListToLeft = () => {
-    if (moviesListRef.current && movieCardRef.current) {
-      const widthCard = movieCardRef.current.offsetWidth + 40;
-      moviesListRef.current.scrollBy({ left: -widthCard!, behavior: "smooth" });
+  const slideItemsListToLeft = () => {
+    if (itemsListRef.current && itemCardRef.current) {
+      const widthCard = itemCardRef.current.offsetWidth + 40;
+      itemsListRef.current.scrollBy({ left: -widthCard!, behavior: "smooth" });
     }
   };
-
 
   return (
     <section>
@@ -46,33 +45,33 @@ export default function MoviesList(props: MoviesListProps) {
         className={`relative after:block after:absolute after:inset-0 after:dark:bg-gradient-btt-h after:pointer-events-none after:z-10 ${classNames}`}
       >
         <ul
-          ref={moviesListRef}
+          ref={itemsListRef}
           className="w-full flex gap-5 overflow-x-auto disable-scrollbar snap-mandatory snap-x justify-items-center"
         >
           {items != null && items.length ? (
             items.map((item) => (
-              <MovieCard
+              <ItemCard
                 key={item.id}
-                movie={item as MovieType}
-                movieCardRef={movieCardRef}
+                item={item as MovieType}
+                itemCardRef={itemCardRef}
               />
             ))
           ) : (
             <>
               {Array.from({ length: 10 }).map((_, index) => (
-                <MovieCard key={index} movieCardRef={movieCardRef} />
+                <ItemCard key={index} itemCardRef={itemCardRef} />
               ))}
             </>
           )}
         </ul>
         <button
-          onClick={slideMoviesListToLeft}
+          onClick={slideItemsListToLeft}
           className="hidden md:block absolute left-10 top-[50%] -translate-y-[50%] z-10 font-bold text-white text-5xl p-5 hover:scale-90 ease-soft-spring transition-transform duration-500 drop-shadow-lg text-w-line"
         >
           {"<-"}
         </button>
         <button
-          onClick={slideMoviesListToRight}
+          onClick={slideItemsListToRight}
           className="hidden md:block absolute right-10 top-[50%] -translate-y-[50%] z-10 font-bold text-white text-5xl p-5 hover:scale-90 ease-soft-spring transition-transform duration-500 drop-shadow-lg text-w-line"
         >
           {"->"}
