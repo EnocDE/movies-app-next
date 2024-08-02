4973e6f6081611d36bfcb800d30bcc4e3a11ff5c
Enoc Dominguez <enocescazan@hotmail.com>
2024-08-02 16:22:39 -0600

    Update props types

--- components/MenuSelectionHeader/index.tsx
+++ components/MenuSelectionHeader/index.tsx
@@ -2,15 +2,15 @@ import { MenuSelectionType } from "@/types/menu-selection";
 import { Dispatch, SetStateAction } from "react";
 
 interface MenuSelectionHeaderProps {
-  setOptionState: Dispatch<SetStateAction<"movie" | "tv">>;
-  optionState: "movie" | "tv";
+  setOptionState: Dispatch<SetStateAction<MenuSelectionType["type"]>>;
+  optionState: MenuSelectionType["type"];
   menuSelectionData: MenuSelectionType[];
 }
 
 export default function MenuSelectionHeader(props: MenuSelectionHeaderProps) {
   const { setOptionState, optionState, menuSelectionData } = props;
 
-  const handleSetOption = (value: "movie" | "tv") => setOptionState(value);
+  const handleSetOption = (value: MenuSelectionType["type"]) => setOptionState(value);
   return (
     <div>
       <header className="mx-auto max-w-[80%] flex gap-10 justify-evenly items-center border-b-1 flex-wrap py-10">
