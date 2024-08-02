88383bdb90144ca8eadd7fdd0c9532166211dd43
Enoc Dominguez <enocescazan@hotmail.com>
2024-08-02 11:34:57 -0600

    Update truncateRating function to check if rating exists before attempting to convert the value

--- utils/index.ts
+++ utils/index.ts
@@ -1,5 +1,8 @@
 export const defaultMoviePoster = "https://images.unsplash.com/photo-1489599849927-2ee91cede3ba?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
 
 export function truncateRating(rating: number) {
-  return rating.toFixed(1) || undefined
+  if (rating) {
+    return rating.toFixed(1)
+  }
+  return undefined
 }
