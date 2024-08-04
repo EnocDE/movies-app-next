33e7f4681b6b56d780815c7e483d0719228f979a
Enoc Dominguez <enocescazan@hotmail.com>
2024-08-04 15:03:15 -0600

    Add component for display elements not found message

---
+++ components/ElementsNotFound/index.tsx
@@ -0,0 +1,7 @@
+export default function ElementsNotFound() {
+  return (
+    <div className="w-full h-[470px] flex justify-center items-center">
+      <p className="font-bold text-medium xl:text-2xl">Elements not found</p>
+    </div>
+  );
+}
