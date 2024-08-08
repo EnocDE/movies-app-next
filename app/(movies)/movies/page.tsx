33c1600e0e8913933af2d450f678228b232b5890
Enoc Dominguez <enocescazan@hotmail.com>
2024-08-08 16:35:03 -0600

    Add filters in movies page

--- app/(movies)/movies/page.tsx
+++ app/(movies)/movies/page.tsx
@@ -1,16 +1,15 @@
 import AllMoviesSeries from "@/components/AllMoviesSeries";
+import Filters from "@/components/Filters";
 import { api } from "@/lib/AxiosConfig";
 import { MoviesSchema } from "@/types/movies-response";
 import { redirect } from "next/navigation";
 
 async function getMovies({ page }: { page: string }) {
-  const url = `/3/discover/movie?page=${page}`;
+  const url = `/3/discover/movie?page=${page}&adults=false`;
   try {
     const response = await api(url);
     if (response.status !== 200) throw new Error("Error fetching movies data");
     const result = MoviesSchema.safeParse(response.data);
-    console.log(result.error?.issues);
-    
     if (!result.success) throw new Error("Error parsing series data");
     return result.data?.results;
   } catch (error) {
@@ -18,12 +17,36 @@ async function getMovies({ page }: { page: string }) {
   }
 }
 
-export default async function Page(params: {searchParams: {page?: string}}) {
+export default async function Page(params: {searchParams: {page?: string, stars?: string, order?: string, year?: string}}) {
   const page = params.searchParams.page || "1"
+
   if (+page > 500) return redirect("/movies")
   const items = await getMovies({page})
 
+  const filterItems = () => {
+    let filterItems = items
+    
+    if (params.searchParams.order) {
+      params.searchParams.order === "ASC" 
+      ? filterItems = filterItems?.toSorted((a, b) => a.title.localeCompare(b.title))
+      : filterItems = filterItems?.toSorted((a, b) => b.title.localeCompare(a.title))
+    }
+    if (params.searchParams.stars) {
+      filterItems = filterItems?.filter(item => item.vote_average > +params.searchParams.stars!)
+    }
+    if (params.searchParams.year && params.searchParams.year !== "All") {
+      filterItems = filterItems?.filter(item => new Date(item.release_date) > new Date(params.searchParams.year!))
+    }
+
+    return filterItems
+  }
+
+  const filteredItems = filterItems()
+
   return (
-    <AllMoviesSeries uknownItems={items} />
+    <>
+      <Filters />
+      <AllMoviesSeries uknownItems={filteredItems} />
+    </>
   )
 }
