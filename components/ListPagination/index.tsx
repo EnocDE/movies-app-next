4330effda58be31365ae962eda4d9cce2adea9ac
Enoc Dominguez <enocescazan@hotmail.com>
2024-08-08 16:25:42 -0600

    Update pagination to preserve all URL parameters when changing pages

--- components/ListPagination/index.tsx
+++ components/ListPagination/index.tsx
@@ -1,17 +1,31 @@
 "use client";
 import { Pagination } from "@nextui-org/react";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
 
 export default function ListPagination() {
+  const params = useSearchParams()
   const router = useRouter();
+  const pathname = usePathname()
   
+  const handleChangePage = (selectedPage: number) => {
+    const searchParams = new URLSearchParams(window.location.search)
+    if (searchParams.get("page")) {
+      searchParams.set("page", selectedPage.toString())
+    } else {
+      searchParams.append("page", selectedPage.toString())
+    }
+    
+    router.replace(`${pathname}?${searchParams}`)
+  }
+
   return (
     <Pagination
-      onChange={(currentPage) => router.replace(`?page=${currentPage}`)}
+      onChange={(selectedPage) => handleChangePage(selectedPage)}
       className="mx-auto w-fit mt-5"
       isCompact
       total={500}
-      initialPage={1}
+      initialPage={+params.get("page")! || 1}
     />
   );
 }
